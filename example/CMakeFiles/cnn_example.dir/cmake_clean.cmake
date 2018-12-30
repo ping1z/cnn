c505113bf070ccca841a073317c381956925f498
ping1z <zhongyp.design@gmail.com>
2018-12-29 19:33:00 -0600

    Integrate with CMake

---
+++ example/CMakeFiles/cnn_example.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cnn_example.dir/main.cpp.o"
+  "../build/cnn_example.pdb"
+  "../build/cnn_example"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cnn_example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
